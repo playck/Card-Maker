f2ad0d438a09145ccdf5ed6c693b34d4d7a15323
playck <playck@naver.com>
2021-05-25 16:15:01 +0900

    project set up & Add: firebase

---
+++ src/service/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBRL9YXHWFxnPs0oUpMlX8aPr58xZGbZOA",
+  authDomain: "business-card-maker-8b23f.firebaseapp.com",
+  projectId: "business-card-maker-8b23f",
+  dataBaseURL: "https://business-card-maker-8b23f-default-rtdb.firebaseio.com/",
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
